0bc48d16f7d8534a7d7ebd925b0c5a6d0c5b85ed
LeonTan828 <yltan828@gmail.com>
2020-05-09 02:16:18 -0500

    fix typing in input and selecting input

--- submitForm.js
+++ submitForm.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 const name = 'Naomi Speiss';
 const title = 'IAM Service Coordinator';
 const email = 'asdf@mail';
+const date = '03/21/20';
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -13,32 +14,41 @@ const email = 'asdf@mail';
   // actions
   console.log('start');
 
+  await page.focus('[name="Kka8Wbd"]');
+  await page.keyboard.type(name);
+
+  await page.focus('[name="8OXLYgM"]');
+  await page.keyboard.type(title);
+
+  await page.focus('[name="Date Worked"]');
+  await page.keyboard.type(date);
+
   await page.click('div[id=yD9p2E1]');
   await page.click('div[id=react-select-2-option-10]');
 
   await page.click('div[id=E5EX6Xe]');
   await page.click('div[id=react-select-3-option-1]');
 
-  await page.click('input[name=EMAIL_RECEIPT_CHECKBOX]');
-  await page.$eval(
-    'input[name=EMAIL_RECEIPT]',
-    (el, email) => (el.value = email),
-    email
-  );
+  await page.click('div[id=eG8ve3p]');
+  await page.click('div[id=react-select-4-option-1]');
 
-  await page.$eval('[name="Kka8Wbd"]', (el, name) => (el.value = name), name);
-  await page.$eval(
-    '[name="8OXLYgM"]',
-    (el, title) => (el.value = title),
-    title
-  );
+  await page.click('[name="EMAIL_RECEIPT_CHECKBOX"]');
+  await page.focus('[name="EMAIL_RECEIPT"]');
+  await page.keyboard.type(email);
 
-  const date = '03/04/2020';
-  await page.$eval(
-    '[name="Date Worked"]',
-    (el, date) => (el.value = date),
-    date
-  );
+  // NOTE
+  // SUBMIT button
+
+  // await page.click('[data-client-id="form_submit_btn"]');
+
+  // try {
+  //   await page.waitFor('[data-client-id="confirmation_msg"]', {
+  //     timeout: 3000
+  //   });
+  //   await page.goto('https://www.google.com/');
+  // } catch (err) {
+  //   console.log('timed out');
+  // }
 
   console.log('got here');
   await browser.waitForTarget(() => false);
