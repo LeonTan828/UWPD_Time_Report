e86f1081fb792ebc8ab52c33ed73e45379977817
LeonTan828 <yltan828@gmail.com>
2020-05-11 11:31:29 -0500

    included other field entries and some commenting

--- submitForm.js
+++ submitForm.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const csv = require('csv-parser');
 const fs = require('fs');
 
+// Variables from command line
 const filename = process.argv[2];
 const name = process.argv[3];
 const title = process.argv[4];
@@ -13,22 +14,68 @@ if (process.argv.length < 6) {
   console.log(email);
 }
 
+// Variables
 let date = [];
+let locations = [];
+let regHours = [];
+let salariedOrHourly = [];
+let otHours = [];
+let rateofOT = [];
+let notes = [];
 let num = 0;
 
+// Read CSV file
 const readFile = () => {
   return new Promise((resolve) => {
     fs.createReadStream(filename)
       .pipe(csv())
       .on('data', async (row) => {
+        // Skip instruction row
+        if (num == 0) {
+          num++;
+          return;
+        }
+
         if (row['Date Worked *']) {
-          if (num == 0) {
-            num++;
-            return;
-          }
           date.push(row['Date Worked *']);
         } else {
-          return;
+          date.push('');
+        }
+
+        if (row['Location of work *']) {
+          locations.push(row['Location of work *']);
+        } else {
+          locations.push('');
+        }
+
+        if (row['Number of Regular Hours in Support of COVID-19']) {
+          regHours.push(row['Number of Regular Hours in Support of COVID-19']);
+        } else {
+          regHours.push('');
+        }
+
+        if (row['Salaried or Hourly *']) {
+          salariedOrHourly.push(row['Salaried or Hourly *']);
+        } else {
+          salariedOrHourly.push('');
+        }
+
+        if (row['Number of Overtime Hours in Support of COVID-19']) {
+          otHours.push(row['Number of Overtime Hours in Support of COVID-19']);
+        } else {
+          otHours.push('');
+        }
+
+        if (row['Rate of Overtime']) {
+          rateofOT.push(row['Rate of Overtime']);
+        } else {
+          rateofOT.push('');
+        }
+
+        if (row['Notes']) {
+          notes.push(row['Notes']);
+        } else {
+          notes.push('');
         }
 
         console.log('new line');
@@ -37,7 +84,8 @@ const readFile = () => {
   });
 };
 
-const entryFunc = (newdate) => {
+// Data Entry function
+const entryFunc = (newdate, loc, reghr, soh, othr, rtot, note) => {
   return new Promise((resolve) => {
     (async () => {
       const browser = await puppeteer.launch({ headless: false });
@@ -45,27 +93,102 @@ const entryFunc = (newdate) => {
       await page.goto(
         'https://app.smartsheet.com/b/form/5784f92c46a14794abc5eb68c420ba7e'
       );
-      // actions
-      console.log('start');
 
+      // Entry starts here
+      console.log('Start entry');
+
+      // Name
       await page.focus('[name="Kka8Wbd"]');
       await page.keyboard.type(name);
 
+      // Job Title
       await page.focus('[name="8OXLYgM"]');
       await page.keyboard.type(title);
 
+      // Organization
+      // Set to DoIT
+      await page.click('[data-client-id="container_Organization"]');
+      await page.click('[id="react-select-2-option-10"]');
+
+      // Date Worked
       await page.focus('[name="Date Worked"]');
       await page.keyboard.type(newdate);
 
-      await page.click('div[id=yD9p2E1]');
-      await page.click('div[id=react-select-2-option-10]');
+      // Location of work
+      await page.click('[data-client-id="container_Location of Work"]');
+      if (loc == 'remote') {
+        await page.click('[id="react-select-3-option-1"]');
+      } else if (loc == 'on campus') {
+        await page.click('[id="react-select-3-option-2"]');
+      } else if (loc == 'state EOC on-site') {
+        await page.click('[id="react-select-3-option-3"]');
+      } else {
+        // None of the input match
+        console.log('something went wrong');
+        console.log(
+          'Input for "location of work" is wrong: ' + loc + ' is not accepted'
+        );
 
-      await page.click('div[id=E5EX6Xe]');
-      await page.click('div[id=react-select-3-option-1]');
+        await browser.close();
+        resolve('not done');
+        return;
+      }
 
-      await page.click('div[id=eG8ve3p]');
-      await page.click('div[id=react-select-4-option-1]');
+      // Number of regular hours
+      await page.focus('[name="jLOv2z0"]');
+      await page.keyboard.type(reghr);
 
+      // Salaried or Hourly
+      await page.click('[data-client-id="container_Salaried or Hourly"]');
+      if (soh == 'salaried') {
+        await page.click('[id="react-select-4-option-1"]');
+      } else if (soh == 'hourly') {
+        await page.click('[id="react-select-4-option-2"]');
+      } else {
+        // None of the input match
+        console.log('something went wrong');
+        console.log(
+          'Input for "salaried or hourly" is wrong: ' + soh + ' is not accepted'
+        );
+
+        await browser.close();
+        resolve('not done');
+        return;
+      }
+
+      // Number of Overtime Hours
+      await page.focus('[name="lLev2w0"]');
+      await page.keyboard.type(othr);
+
+      // Rate of Overtime
+      await page.click('[data-client-id="container_Rate of Overtime"]');
+      if (rtot == 'x1.0') {
+        await page.click('[id="react-select-5-option-1"]');
+      } else if (rtot == 'x1.5') {
+        await page.click('[id="react-select-5-option-2"]');
+      } else if (rtot == 'x2.0') {
+        await page.click('[id="react-select-5-option-3"]');
+      } else if (rtot == 'salaried') {
+        await page.click('[id="react-select-5-option-4"]');
+      } else if (rtot == '') {
+        await page.click('[id="react-select-5-option-5"]');
+      } else {
+        // None of the input match
+        console.log('something went wrong');
+        console.log(
+          'Input for "rate of overtime" is wrong: ' + rtot + ' is not accepted'
+        );
+
+        await browser.close();
+        resolve('not done');
+        return;
+      }
+
+      // Notes
+      await page.focus('[name="arXqkjK"]');
+      await page.keyboard.type(note);
+
+      // Submit a copy of response
       await page.click('[name="EMAIL_RECEIPT_CHECKBOX"]');
       await page.focus('[name="EMAIL_RECEIPT"]');
       await page.keyboard.type(email);
@@ -100,8 +223,22 @@ const mainFunc = async () => {
   let length = date.length;
   console.log(length);
   console.log(date);
+  console.log(locations);
+  console.log(regHours);
+  console.log(salariedOrHourly);
+  console.log(otHours);
+  console.log(rateofOT);
+  console.log(notes);
   for (let i = 0; i < length; i++) {
-    await entryFunc(date[i]);
+    await entryFunc(
+      date[i],
+      locations[i],
+      regHours[i],
+      salariedOrHourly[i],
+      otHours[i],
+      rateofOT[i],
+      notes[i]
+    );
     console.log('end of loop here');
   }
 };
